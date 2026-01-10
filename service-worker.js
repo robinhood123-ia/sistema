ab173830e4bf28b795827cc3d9a0fffa17d5af85
Robinson lopez <robinsonlopez1199@gmail.com>
2026-01-10 02:40:44 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker: Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker: Activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  // Cache-first strategy puede añadirse aquí
+});
